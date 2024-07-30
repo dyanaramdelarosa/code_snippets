3b9a18a8b2c9fb91751c0017189fd58416d39627
dyanaramdelarosa <dyanaramdelarosa@gmail.com>
2024-07-30 19:37:49 +0800

    Add solution for Problem 11: Container With Most Water

--- leetcode_problems/container_with_most_water.py
+++ leetcode_problems/container_with_most_water.py
@@ -4,24 +4,21 @@
 
 
 class Solution:
-    def compute_area(self, x0: int, x1: int, y0: int, y1: int) -> int:
+    def computeArea(self, x0: int, x1: int, y0: int, y1: int) -> int:
         return (x1 - x0) * min(y0, y1)
 
-    def maxArea_brute_force(self, height: List[int]) -> int:
+    def maxAreaBruteForce(self, height: List[int]) -> int:
         max_area = 0
         for i in range(len(height)):
             for j in range(i+1, len(height)):
-                computed_area = self.compute_area(i, j, height[i], height[j])
-                if max_area < computed_area:
-                    max_area = computed_area
+                max_area = max(max_area, self.computeArea(i, j, height[i], height[j]))
 
         return max_area
 
     def maxArea(self, height: List[int]) -> int:
         max_area, i, j = 0, 0, len(height)-1
         while i<j:
-            area = self.compute_area(i, j, height[i], height[j])
-            if max_area < area: max_area = area
+            max_area = max(max_area, self.computeArea(i, j, height[i], height[j]))
             if height[i] <= height[j]:
                 i+=1
             else:
