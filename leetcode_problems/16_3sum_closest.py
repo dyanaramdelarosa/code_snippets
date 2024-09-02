dac0a971561172bfa9e9ce516a28bfee224845b4
dyanaramdelarosa <dyanaramdelarosa@gmail.com>
2024-09-02 18:21:57 +0800

    Update solution for Problem 16: 3sum Closest

--- leetcode_problems/16_3sum_closest.py
+++ leetcode_problems/16_3sum_closest.py
@@ -3,27 +3,23 @@
 # Date: September 2, 2024
 
 class Solution:
-    def get_total(self, nums):
-        return sum(nums)
-
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        total = self.get_total(nums[:3])
-        nums = sorted(nums)
+        total = float('inf')
+        nums.sort()
         nums_len = len(nums)
 
         for i in range(0, nums_len):
-            j = i+1
-            k = nums_len-1
+            left = i + 1
+            right = nums_len - 1
 
-            while j<k:
-                cur_total = self.get_total([nums[i], nums[j], nums[k]])
+            while left < right:
+                cur_total = nums[i] + nums[left] + nums[right]
                 if cur_total == target:
                     return cur_total
                 if abs(cur_total - target) < abs(total - target):
                     total = cur_total
                 if cur_total < target:
-                    j += 1
+                    left += 1
                 else:
-                    k -=1
-        i += 1
+                    right -= 1
         return total
