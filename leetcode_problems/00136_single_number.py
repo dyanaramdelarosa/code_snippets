bf8141784e255a8c7a80bc507114e2df4e9f3bc4
Dyanara Dela Rosa <dyanaramdelarosa@gmail.com>
2025-02-12 18:34:52 +0800

    Add solution for Problem 136: Single Number

---
+++ leetcode_problems/00136_single_number.py
@@ -0,0 +1,11 @@
+# Problem Link: https://leetcode.com/problems/valid-palindrome/
+# Author: Dyanara Dela Rosa
+# Date: August 14, 2024
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        no_duplicate = 0
+        for num in nums:
+            no_duplicate = no_duplicate^num
+        return no_duplicate
