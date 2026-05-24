8ae4eacbfad0844cdb7cb81bd2f91ae303badba8
Dyanara Dela Rosa <dyanaramdelarosa@gmail.com>
2026-05-24 17:42:45 +0800

    Add solution for Problem 193: Valid phone numbers

---
+++ leetcode_problems/00193_valid_phone_numbers.sh
@@ -0,0 +1,6 @@
+# Problem Link: https://leetcode.com/problems/valid-phone-numbers/
+# Author: Dyanara Dela Rosa
+# Date: May 24, 2026
+
+
+grep -E "^(\([0-9]{3}\)\s|[0-9]{3}-)[0-9]{3}-[0-9]{4}$" file.txt
