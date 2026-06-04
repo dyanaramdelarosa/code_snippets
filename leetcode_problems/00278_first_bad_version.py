9b46b3117aa07234832d4c2b4abd906e75e5d9a7
Dyanara Dela Rosa <dyanaramdelarosa@gmail.com>
2026-06-04 23:08:46 +0800

    Add solution for Problem 278: First bad version

---
+++ leetcode_problems/00278_first_bad_version.py
@@ -0,0 +1,20 @@
+# Problem Link: https://leetcode.com/problems/first-bad-version/
+# Author: Dyanara Dela Rosa
+# Date: June 4, 2026
+#
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
